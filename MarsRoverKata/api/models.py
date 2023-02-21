da19e7a7f0d0a6ea29dc91ee622ba21ab97300a6
Larbi Gharib <larbizard@gmail.com>
2023-02-21 14:19:08 +0100

    Added obstacles detection

---
+++ MarsRoverKata/api/models.py
@@ -0,0 +1,155 @@
+from django.db import models
+from django.core.exceptions import FieldError
+
+
+class Planet(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=50)
+    size = models.IntegerField() # The size is the integer that defines the amount of latitudes and longitudes of the planet
+
+
+class Rover(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=50)
+    positionX = models.IntegerField()
+    positionY = models.IntegerField()
+    direction = models.CharField(max_length=1)
+    planet = models.ForeignKey(Planet, on_delete=models.CASCADE)
+
+    def getNextPosition(self, forward):
+        if forward:
+            if self.direction == 'N':
+                if self.positionY == 1:
+                    return (self.positionX, self.positionY)
+                else:
+                    return (self.positionX, self.positionY - 1)
+
+            elif self.direction == 'S':
+                if self.positionY == self.planet.size:
+                    return (self.positionX, self.positionY)
+                else:
+                    return (self.positionX, self.positionY + 1)
+
+            elif self.direction == 'E':
+                if self.positionX == self.planet.size:
+                    return (1, self.positionY)
+                else:            
+                    self.positionX = self.positionX + 1
+                    return (self.positionX + 1, self.positionY)
+
+            elif self.direction == 'W':
+                if self.positionX == 1:
+                    return (self.planet.size, self.positionY)
+                else:
+                    return (self.positionX - 1, self.positionY)        
+            else:
+                raise FieldError
+        else:
+            if self.direction == 'N':
+                if self.positionY == self.planet.size:
+                    return (self.positionX, self.positionY)
+                else:
+                    return (self.positionX, self.positionY + 1)
+
+            elif self.direction == 'S':
+                if self.positionY == 1:
+                    return (self.positionX, self.positionY)
+                else:
+                    return (self.positionX, self.positionY - 1)
+
+            elif self.direction == 'E':
+                if self.positionX == 1:
+                    return (self.planet.size, self.positionY)
+                else:
+                    return (self.positionX - 1, self.positionY)
+
+            elif self.direction == 'W':
+                if self.positionX == self.planet.size:
+                    return (1, self.positionY)
+                else:
+                    return (self.positionX + 1, self.positionY)
+            else:
+                raise FieldError
+
+    def checkForObstacle(self, coordinates):
+        if len(Obstacle.objects.filter(positionX=coordinates[0], positionY=coordinates[1])) >= 1:
+            return True
+        else:
+            return False
+
+    def followSequence(self, sequence):
+        for s in sequence:
+            print(f'Current position: ({self.positionX}, {self.positionY}) direction {self.direction}')
+            if s == 'l':
+                self.turnLeft()
+            elif s == 'r':
+                self.turnRight()
+            elif s == 'f':
+                self.moveForward()
+            elif s == 'b':
+                self.moveBackward()
+            else:
+                raise FieldError
+
+    def moveForward(self):
+        print('Moving Forward')
+        (x, y) = self.getNextPosition(True)
+        if self.checkForObstacle((x, y)):
+            print(f'Warning obstacle detected at position ({x}, {y}) aborting sequence!')
+        else:
+            self.positionX = x
+            self.positionY = y
+
+
+            
+    def moveBackward(self):
+        print('Moving Backward')
+        (x, y) = self.getNextPosition(False)
+        if self.checkForObstacle((x, y)):
+            print(f'Warning obstacle detected at position ({x}, {y}) aborting sequence!')
+        else:
+            self.positionX = x
+            self.positionY = y
+
+
+    def turnLeft(self):
+        print('Turning Left')
+        if self.direction == 'N':
+            self.direction = 'W'
+
+        elif self.direction == 'S':
+            self.direction = 'E'
+
+        elif self.direction == 'E':
+            self.direction = 'N'
+
+        elif self.direction == 'W':
+            self.direction = 'S'
+        else:
+            raise FieldError
+
+    def turnRight(self):
+        print('Turning Right')
+        if self.direction == 'N':
+            self.direction = 'E'
+
+        if self.direction == 'S':
+            self.direction = 'W'
+
+        if self.direction == 'E':
+            self.direction = 'S'
+
+        if self.direction == 'W':
+            self.direction = 'N'
+
+    def isAtEdge(self, position):
+        if position == self.planet.size or position == 1:
+            return True
+        return False
+
+class Obstacle(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=50)
+    positionX = models.IntegerField()
+    positionY = models.IntegerField()
+    planet = models.ForeignKey(Planet, on_delete=models.CASCADE)
---
+++ MarsRoverKata/api/tests.py
@@ -0,0 +1,58 @@
+from django.test import TestCase
+from .models import Planet
+from .models import Rover
+from .models import Obstacle
+
+
+class RoverTestCase(TestCase):
+    def setUp(self):
+        planet1 = Planet.objects.create(name="Mars", size=8)
+        Rover.objects.create(name="Wall-e", positionX=4, positionY=1, planet=planet1, direction='N')
+        Rover.objects.create(name="Armstrong", positionX=8, positionY=1, planet=planet1, direction='N')
+        Rover.objects.create(name="Perseverance", positionX=3, positionY=3, planet=planet1, direction='N')
+        Obstacle.objects.create(name="TheRock", positionX=3, positionY=5, planet=planet1)
+
+    def test_rover_is_at_edge(self):
+        rover = Rover.objects.get(name="Armstrong")
+        self.assertTrue(rover.isAtEdge(position=rover.positionX))
+        self.assertTrue(rover.isAtEdge(position=rover.positionY))
+
+    def test_rover_is_at_not_edge(self):
+        rover = Rover.objects.get(name="Wall-e")
+        self.assertFalse(rover.isAtEdge(position=rover.positionX))
+        self.assertTrue(rover.isAtEdge(position=rover.positionY))
+
+    def test_move_rover_to_position(self):
+        """
+            Make the robot move following this sequence lfffrbb starting at position (4, 1) 
+            expected destination is (1, 3)
+        """
+
+        sequence = "lfffrbb"
+
+        rover = Rover.objects.get(name="Wall-e")
+        print(f'Starting position: ({rover.positionX}, {rover.positionY}) direction {rover.direction}')
+        rover.followSequence(sequence)
+
+        print(f'Arriving position: ({rover.positionX}, {rover.positionY}) direction {rover.direction}')
+        self.assertEqual(rover.positionX, 1)
+        self.assertEqual(rover.positionY, 3)
+    
+    def test_move_rover_to_position_with_obstacle(self):
+        """
+         Make the robot move following this sequence llffrbb starting at position (3, 3) 
+         expected to see obstacle at position (3, 5) after turning left twice and moving forward
+        """
+
+        sequence = "llffrbb"
+
+        rover = Rover.objects.get(name="Perseverance")
+
+        # Obstacle is at position (3, 5)
+
+        print(f'Starting position: ({rover.positionX}, {rover.positionY}) direction {rover.direction}')
+        rover.followSequence(sequence)
+
+        print(f'Arriving position: ({rover.positionX}, {rover.positionY}) direction {rover.direction}')
+        self.assertEqual(rover.positionX, 3)
+        self.assertEqual(rover.positionY, 4)
