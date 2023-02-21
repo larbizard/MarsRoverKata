97248e83729bcbc2a1b502e859184d03bd72820d
Larbi Gharib <larbizard@gmail.com>
2023-02-21 10:45:42 +0100

    Added Rover moving functions

--- api/models.py
+++ api/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-class Rover:
+class Rover(models.Model):
     DIRECTION = [
         ('N', 'North'),
         ('S', 'South'),
@@ -9,18 +9,69 @@ class Rover:
     ]
     id = models.AutoField()
     name = models.CharField(max_length=50)
-    latitude = models.FloatField()
-    longitude = models.FloatField()
+    positionX = models.FloatField()
+    positionY = models.FloatField()
     direction = models.CharField(choices=DIRECTION)
 
-    def moveForward():
+    def moveForward(self):
         print('Moving Forward')
+        if self.direction == 'N':
+            self.positionY = self.positionY - 1
 
-    def moveBackward():
-        print('Moving Forward')
+        if self.direction == 'S':
+            self.positionY = self.positionY + 1
 
-    def turnLeft():
+        if self.direction == 'E':
+            self.positionX = self.positionX + 1
+
+        if self.direction == 'W':
+            self.positionX = self.positionX - 1            
+
+    def moveBackward(self):
+        print('Moving Backward')
+        if self.direction == 'N':
+            self.positionY = self.positionY + 1
+
+        if self.direction == 'S':
+            self.positionY = self.positionY - 1
+
+        if self.direction == 'E':
+            self.positionX = self.positionX - 1
+
+        if self.direction == 'W':
+            self.positionX = self.positionX + 1        
+
+    def turnLeft(self):
         print('Turning Left')
+        if self.direction == 'N':
+            self.direction = 'W'
 
-    def turnRight():
+        if self.direction == 'S':
+            self.direction = 'E'
+
+        if self.direction == 'E':
+            self.direction = 'N'
+
+        if self.direction == 'W':
+            self.direction = 'S'
+
+
+    def turnRight(self):
         print('Turning Right')
+        if self.direction == 'N':
+            self.direction = 'E'
+
+        if self.direction == 'S':
+            self.direction = 'W'
+
+        if self.direction == 'E':
+            self.direction = 'S'
+
+        if self.direction == 'W':
+            self.direction = 'N'
+
+class Obstacle(models.Model):
+    id = models.AutoField()
+    name = models.CharField(max_length=50)
+    positionX = models.FloatField()
+    positionY = models.FloatField()
